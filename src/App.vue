d8d86af749be11b6a4a092694116c855ae3b70d6
Daniel Gee <dan@dan-gee.co.uk>
2022-12-07 06:22:12 +0000

    Add code

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,42 @@
 <template>
-  <div class="w-full h-full flex justify-center items-center">
-    <span class="text-3xl">Good luck!</span>
+  <div class="w-full h-full flex flex-col justify-center items-center">
+    <div class="text-3xl" v-if="jokeState !== 'gettingJoke'">{{ jokeSetup }}</div>
+    <div class="text-3xl" v-if="jokeState === 'answer'">{{ jokeResponse }}</div>
+    <button
+      class="px-4 py-2 font-semibold text-sm bg-cyan-500 text-white rounded-full shadow-sm"
+      @click="getAnswer()"
+      v-if="jokeState === 'newJoke'"
+    >
+      Tell Me!
+    </button>
+    <button
+      class="px-4 py-2 font-semibold text-sm bg-cyan-500 text-white rounded-full shadow-sm"
+      @click="getNewJoke()"
+      v-if="jokeState === 'answer'"
+    >
+      New Joke
+    </button>
   </div>
 </template>
 
 <script setup>
-// fetch('https://v2.jokeapi.dev/joke/christmas') ...
+import { ref } from 'vue'
+const jokeState = ref('gettingJoke')
+
+async function getNewJoke() {
+  jokeState.value = 'gettingJoke'
+  const result = await (await fetch('https://v2.jokeapi.dev/joke/christmas')).json()
+  jokeSetup.value = result.setup
+  jokeResponse.value = result.delivery
+  jokeState.value = 'newJoke'
+}
+
+function getAnswer() {
+  jokeState.value = 'answer'
+}
+
+const jokeSetup = ref('')
+const jokeResponse = ref('')
+
+getNewJoke()
 </script>
